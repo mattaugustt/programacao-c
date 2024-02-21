4b1f2e5233470f1deb064e5178de6dddaa28c7cf
Matheus Augusto <122750956+mattaugustt@users.noreply.github.com>
2024-02-20 22:24:14 -0300

    instruções das tarefas e as tarefas em si

---
+++ Tarefa 9 - my_getline.c
@@ -0,0 +1,33 @@
+﻿Matheus Augusto de Souza
+mataugusto1999@gmail.com
+
+
+#include <stdio.h>
+#include <string.h>
+#include "getline.h"
+
+
+#define OK 0
+#define NO_INPUT 1
+#define TOO_LONG 2
+
+
+int my_getline(char *prmpt, char *buff, size_t sz, FILE *fp) {
+    int ch, extra;
+    if (prmpt != NULL) {
+        printf ("%s", prmpt);
+        fflush (stdout);
+    }
+    if (fgets (buff, sz, fp) == NULL)  // ao inves de ter o stdin, usamos o arquivo fp.
+        return NO_INPUT;
+
+
+        if (buff[strlen(buff)-1] != '\n') {
+        extra = 0;
+        while (((ch = getchar()) != '\n') && (ch != EOF))
+            extra = 1;
+        return (extra == 1) ? TOO_LONG : OK;
+    }
+    buff[strlen(buff)-1] = '\0';
+    return OK;
+}
---
+++ Tarefa 9 - read-events.c
@@ -0,0 +1,57 @@
+﻿Matheus Augusto de Souza
+mataugusto1999@gmail.com
+
+
+#include <stdio.h>
+#include <string.h>
+#include "getline.h"
+
+
+#define MAXLINE 1000
+
+
+char *month(int m) {
+  char *names[] = {
+    "janeiro", "fevereiro", "março", "abril",
+    "maio", "junho", "julho", "agosto",
+    "setembro", "outubro", "novembro", "dezembro"
+  };
+  return names[m - 1];
+}
+
+
+int main(int argc, char *argv[]) {
+  int d, m, y; int r;
+  char ln[MAXLINE];
+
+
+  if (argc > 1)  // caso o usuario digite algo alem da função.
+  {
+    FILE *fp;  // caso o usuario utilize de fato um arquivo.
+    fp = fopen(argv[1], "r");
+    for (;;) {
+      r = my_getline(NULL, ln, sizeof ln, fp);
+      if (r == TOO_LONG) {
+        printf("too long: [%s]\n", ln);
+        continue;
+      }
+    
+  
+      if (r == NO_INPUT) {/* quando encontramos o fim do arquivo */
+        break;
+      }
+      
+      char event[MAXLINE]; memset(event, '\0', sizeof event);
+      r = sscanf(ln, "%d/%d/%d %[^\n]", &d, &m, &y, event);
+      if (r < 3) { //Invalid line for some reason. 
+        continue;
+      }
+    
+      char date[MAXLINE]; memset(date, '\0', sizeof date);
+      snprintf(date, sizeof date, "Dia %d de %s de %d", d, month(m), y);
+      printf("%-30s --> %-.30s%s\n", date, event, 
+            strlen(event) > 30 ? "..." : "");
+    }
+    fclose(fp);
+  }
+}
